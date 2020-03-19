d2fa592323f4c7fef77d54bcc5f150f57ff6dbf6
Bruno Flores <bruno@brunoflores.com.br>
2020-03-20 09:27:25 +1000

    Upgrade dependencies. WIP.

--- floud-k8s-image-provisioner.sh
+++ floud-k8s-image-provisioner.sh
@@ -23,31 +23,31 @@ sudo mv cfssl cfssljson /usr/local/bin/
 
 # etcd
 wget -q --show-progress --https-only --timestamping \
-    "https://github.com/etcd-io/etcd/releases/download/v3.4.0/etcd-v3.4.0-linux-amd64.tar.gz"
-tar -xvf etcd-v3.4.0-linux-amd64.tar.gz
-sudo mv etcd-v3.4.0-linux-amd64/etcd* /usr/local/bin/
+    "https://github.com/etcd-io/etcd/releases/download/v3.4.5/etcd-v3.4.5-linux-amd64.tar.gz"
+tar -xvf etcd-v3.4.5-linux-amd64.tar.gz
+sudo mv etcd-v3.4.5-linux-amd64/etcd* /usr/local/bin/
 sudo mkdir -p /etc/etcd /var/lib/etcd
 
 # Kubernetes Control Plane
 sudo mkdir -p /etc/kubernetes/config
 sudo mkdir -p /var/lib/kubernetes
 wget -q --show-progress --https-only --timestamping \
-    "https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kube-apiserver" \
-    "https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kube-controller-manager" \
-    "https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kube-scheduler" \
-    "https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kubectl"
+    "https://storage.googleapis.com/kubernetes-release/release/v1.17.0/bin/linux/amd64/kube-apiserver" \
+    "https://storage.googleapis.com/kubernetes-release/release/v1.17.0/bin/linux/amd64/kube-controller-manager" \
+    "https://storage.googleapis.com/kubernetes-release/release/v1.17.0/bin/linux/amd64/kube-scheduler" \
+    "https://storage.googleapis.com/kubernetes-release/release/v1.17.0/bin/linux/amd64/kubectl"
 chmod +x kube-apiserver kube-controller-manager kube-scheduler kubectl
 sudo mv kube-apiserver kube-controller-manager kube-scheduler kubectl /usr/local/bin/
 
 # Kubernetes Nodes
 wget -q --show-progress --https-only --timestamping \
-    "https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.15.0/crictl-v1.15.0-linux-amd64.tar.gz" \
-    "https://github.com/opencontainers/runc/releases/download/v1.0.0-rc8/runc.amd64" \
-    "https://github.com/containernetworking/plugins/releases/download/v0.8.2/cni-plugins-linux-amd64-v0.8.2.tgz" \
-    "https://github.com/containerd/containerd/releases/download/v1.2.9/containerd-1.2.9.linux-amd64.tar.gz" \
-    "https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kubectl" \
-    "https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kube-proxy" \
-    "https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kubelet"
+    "https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.17.0/crictl-v1.17.0-linux-amd64.tar.gz"
+    "https://github.com/opencontainers/runc/releases/download/v1.0.0-rc10/runc.amd64" \
+    "https://github.com/containernetworking/plugins/releases/download/v0.8.5/cni-plugins-linux-amd64-v0.8.5.tgz" \
+    "https://github.com/containerd/containerd/releases/download/v1.2.13/containerd-1.2.13.linux-amd64.tar.gz" \
+    "https://storage.googleapis.com/kubernetes-release/release/v1.17.0/bin/linux/amd64/kubectl" \
+    "https://storage.googleapis.com/kubernetes-release/release/v1.17.0/bin/linux/amd64/kube-proxy" \
+    "https://storage.googleapis.com/kubernetes-release/release/v1.17.0/bin/linux/amd64/kubelet"
 # Create installation dirs
 sudo mkdir -p \
     /etc/cni/net.d \
@@ -58,10 +58,10 @@ sudo mkdir -p \
     /var/run/kubernetes
 # Install
 mkdir containerd
-tar -xvf crictl-v1.15.0-linux-amd64.tar.gz
-tar -xvf containerd-1.2.9.linux-amd64.tar.gz -C containerd
+tar -xvf crictl-v1.17.0-linux-amd64.tar.gz
+tar -xvf containerd-1.2.13.linux-amd64.tar.gz -C containerd
 sudo mv containerd/bin/* /bin/
-sudo tar -xvf cni-plugins-linux-amd64-v0.8.2.tgz -C /opt/cni/bin/
+sudo tar -xvf cni-plugins-linux-amd64-v0.8.5.tgz -C /opt/cni/bin/
 sudo mv runc.amd64 runc
 chmod +x crictl kubectl kube-proxy kubelet runc
 sudo mv crictl kubectl kube-proxy kubelet runc /usr/local/bin/
